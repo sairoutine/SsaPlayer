d9fdeecb9c559ec3ecef877c6d1b98faef712fb4
sairoutine <sairoutine@gmail.com>
2017-09-26 22:57:09 +0900

    move: license message to readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # SsaPlayer
+ Ss5ConverterToSSAJSON v1.0.3
+
+ Copyright(C) Web Technology Corp.
+ http://www.webtech.co.jp/
+
+ Ss5ConverterToSSAJSON
+ https://github.com/SpriteStudio/Ss5ConverterToSSAJSON/wiki
+ から出力したJSONファイルをHTML5で再生するサンプルプログラムです。
--- SsaPlayer.js
+++ SsaPlayer.js
@@ -1,15 +1,3 @@
-//-----------------------------------------------------------
-// Ss5ConverterToSSAJSON v1.0.3
-//
-// Copyright(C) Web Technology Corp.
-// http://www.webtech.co.jp/
-//
-// Ss5ConverterToSSAJSON
-// https://github.com/SpriteStudio/Ss5ConverterToSSAJSON/wiki
-// から出力したJSONファイルをHTML5で再生するサンプルプログラムです。
-//
-//-----------------------------------------------------------
-
 ////////////////////////////////////////////////////////////
 // SsImageList
 ////////////////////////////////////////////////////////////
