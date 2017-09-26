e5b1b97ba0dfdaecda67cddfa4047d8431d1dc9d
sairoutine <sairoutine@gmail.com>
2017-09-26 23:42:41 +0900

    refactor SsImageList class

--- SsImageList.js
+++ SsImageList.js
@@ -1,59 +1,71 @@
 'use strict';
 
-function SsImageList(imageFiles, aFileRoot, loadImmediately, aOnLoad) {
+function SsImageList(imageFiles, aFileRoot, loadImmediately, aOnLoadCallBack) {
 
-	this.fileRoot = aFileRoot;
-	this.imagePaths = [];
-	this.images = [];
+	// 画像パスのroot
+	this._fileRoot = aFileRoot;
+
+	this._imagePaths = [];
+	this._images = []; // Image インスタンスの配列
 
 	// ロード完了時に呼ばれるコールバック
 	// Callback that is called when the load is finished.
-	this.onLoad = aOnLoad;
-
-	// 全部読み込まれた場合のみユーザーが設定したコールバックを呼ぶ
-	// Only when it is all loaded, is called a callback set by the user.
-	this.onLoad_ = function () {
-		for (var i in this.images)
-			if (i !== null && i.complete === false)
-				return;
-		if (this.onLoad)
-			this.onLoad();
-	};
+	this._onLoadCallBack = aOnLoadCallBack;
 
 	for (var i = 0; i < imageFiles.length; i++) {
-		var path = this.fileRoot + imageFiles[i];
-//        console.log(path);
-		this.imagePaths.push(path);
+		var path = this._fileRoot + imageFiles[i];
+
+		this._imagePaths.push(path);
 		var image = new Image();
-		if (loadImmediately)
-		{
-			image.onload = this.onLoad_;
+		if (loadImmediately) {
+			image.onload = this._onLoad.bind(this);
 			image.src = path;
 		}
-		this.images.push(image);
+		this._images.push(image);
 	}
 }
 
 // 指定したインデックスのImageを返す
 // Get image at specified index.
 SsImageList.prototype.getImage = function (index) {
-	if (index < 0 || index >= this.images.length) return null;
-	return this.images[index];
+	if (index < 0 || index >= this._images.length) return null;
+	return this._images[index];
 };
 
 // 指定したインデックスの画像をimagePathで差し替える。
 // Replace image of specified index at imagePath.
 SsImageList.prototype.setImage = function (index, imagePath) {
-	if (index < 0 || index >= this.images.length) return null;
-	this.imagePaths[index] = this.fileRoot + imagePath;
-	this.images[index].onload = this.onLoad_;
-	this.images[index].src = this.imagePaths[index];
+	if (index < 0 || index >= this._images.length) return null;
+	this._imagePaths[index] = this._fileRoot + imagePath;
+	this._images[index].onload = this._onLoad.bind(this);
+	this._images[index].src = this._imagePaths[index];
 };
 
 // ロード完了時コールバックを設定する
 // Set a callback when load is finished.
 SsImageList.prototype.setOnLoad = function (cb) {
-	this.onLoad = cb;
+	this._onLoadCallBack = cb;
 };
+
+// 全部読み込まれた場合のみユーザーが設定したコールバックを呼ぶ
+// Only when it is all loaded, is called a callback set by the user.
+SsImageList.prototype._onLoad = function () {
+	for (var image in this._images) {
+		if (image !== null && image.complete === false) {
+			return;
+		}
+	}
+
+	if (this._onLoadCallBack) {
+		this._onLoadCallBack();
+	}
+};
+
+
+
+
+
+
+
 
 module.exports = SsImageList;
